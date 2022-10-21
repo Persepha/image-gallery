8e4ff0d3a5509e91c8a8487e2675fa98b6183471
Vasilevich Dmitry <vasilevich.dmt@gmail.com>
2022-10-21 21:55:38 +0300

    Add common.models.TimeStampedModel

---
+++ web/common/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self
+    updating ``created`` and ``modified`` fields.
+    """
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
